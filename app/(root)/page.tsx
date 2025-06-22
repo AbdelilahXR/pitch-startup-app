c233325fc0f337afb0e5e554ac3b8039724c5bd9
Abdelilah ETTARCH <125505938+AbdelilahXR@users.noreply.github.com>
2025-06-22 20:49:26 +0100

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+export default function Home() {
+    console.log("what am i doing here - server / client? ");
+  return (
+      <>
+      <h1 className="text-2xl">Home</h1>
+      </>
+      
+    
+    
+  );
+}
---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div>
+        <div>_______________</div>
+    </div>
+  )
+}
+
+export default loading
